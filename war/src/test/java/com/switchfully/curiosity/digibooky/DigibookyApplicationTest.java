ab2efde70396a44ab91d9c537e11afa958ec3854
LioKr <lio.krebs@gmail.com>
2021-03-24 10:07:09 +0100

    0 initialisation : project structure defined

---
+++ war/src/test/java/com/switchfully/curiosity/digibooky/DigibookyApplicationTest.java
@@ -0,0 +1,17 @@
+package com.switchfully.curiosity.digibooky;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DigibookyApplicationTest {
+    @Test
+    void contextTest() {
+        //GIVEN
+
+        //WHEN
+
+        //THEN
+
+    }
+}
