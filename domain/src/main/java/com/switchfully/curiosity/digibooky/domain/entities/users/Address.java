4fd9815b63698f15c393dd07d4f0db71e17f3b3b
LaurentLch <laurentlonchay@gmail.com>
2021-03-25 11:59:09 +0100

    06A | In progress | Implementation of tests & validation of user

--- domain/src/main/java/com/switchfully/curiosity/digibooky/domain/entities/users/Address.java
+++ domain/src/main/java/com/switchfully/curiosity/digibooky/domain/entities/users/Address.java
@@ -7,9 +7,14 @@ public final class Address {
     private final String city;
 
     public Address(String streetName, String streetNumber, String postCode, String city) {
+        if (!isValidAddress(city)) throw new IllegalArgumentException("Illegal argument was given");
         this.streetName = streetName;
         this.streetNumber = streetNumber;
         this.postCode = postCode;
         this.city = city;
+    }
+
+    public static boolean isValidAddress(String city) {
+        return city!=null;
     }
 }
--- domain/src/main/java/com/switchfully/curiosity/digibooky/domain/entities/users/User.java
+++ domain/src/main/java/com/switchfully/curiosity/digibooky/domain/entities/users/User.java
@@ -3,6 +3,7 @@ package com.switchfully.curiosity.digibooky.domain.entities.users;
 import java.util.UUID;
 
 public class User {
+    private static final String EMAIL_FORMAT = "^\\w+@\\w+\\.\\w+$";
     private final UUID id;
     private final String inss;    //TODO make value object
     private final String lastname;
@@ -10,7 +11,11 @@ public class User {
     private final String email;    //TODO make value object
     private final Address address;
 
+
     public User(String inss, String lastname, String firstname, String email, Address address) {
+        if (!isValidUser(inss, lastname, email, address)) {
+            throw new IllegalArgumentException("Illegal Arguments");
+        }
         this.id = UUID.randomUUID();
         this.inss = inss;
         this.lastname = lastname;
@@ -19,4 +24,11 @@ public class User {
         this.address = address;
     }
 
+    public static boolean isValidUser(String inss, String lastname, String email, Address address) {
+        return  inss != null
+                && lastname != null
+                && email != null
+                && email.matches(EMAIL_FORMAT)
+                && address != null;
+    }
 }
