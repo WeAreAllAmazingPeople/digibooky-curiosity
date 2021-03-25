f36290ad5e2e8e0a658d4bdaeb9ccbe48700ecb4
LaurentLch <laurentlonchay@gmail.com>
2021-03-25 14:34:47 +0100

    06A | Setup | Created Interfaces

---
+++ domain/src/main/java/com/switchfully/curiosity/digibooky/domain/database/UserDatabase.java
@@ -0,0 +1,2 @@
+package com.switchfully.curiosity.digibooky.domain.database;public interface UserDatabase {
+}
---
+++ service/src/main/java/com/switchfully/curiosity/digibooky/service/UserService.java
@@ -0,0 +1,2 @@
+package com.switchfully.curiosity.digibooky.service;public interface UserService {
+}
