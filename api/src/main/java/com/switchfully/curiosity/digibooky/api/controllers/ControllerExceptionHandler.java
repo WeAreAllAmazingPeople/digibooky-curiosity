357e0916c067ccf668b984dc7e6308be14ae2c54
LaurentLch <laurentlonchay@gmail.com>
2021-03-26 11:19:19 +0100

    06A | In progress | Merge

--- api/src/main/java/com/switchfully/curiosity/digibooky/api/controllers/ControllerExceptionHandler.java
+++ api/src/main/java/com/switchfully/curiosity/digibooky/api/controllers/ControllerExceptionHandler.java
@@ -1,5 +1,7 @@
 package com.switchfully.curiosity.digibooky.api.controllers;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
@@ -9,8 +11,11 @@ import java.io.IOException;
 @ControllerAdvice // Class that offers methods to all controllers
 public class ControllerExceptionHandler {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(ControllerExceptionHandler.class);
+
     @ExceptionHandler(IllegalArgumentException.class)
-    public void handleIllegalArgumenbtException(IllegalArgumentException exception, HttpServletResponse response) throws IOException{
+    public void handleIllegalArgumentException(IllegalArgumentException exception, HttpServletResponse response) throws IOException{
+        LOGGER.warn(exception.getMessage(), exception);
         response.sendError(HttpServletResponse.SC_BAD_REQUEST, exception.getMessage());
     }
 }
