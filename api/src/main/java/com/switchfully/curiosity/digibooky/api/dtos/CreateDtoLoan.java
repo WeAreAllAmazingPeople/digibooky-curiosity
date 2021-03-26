d1bbd74411b64782a28310b8b5e94589331fefcd
LaurentLch <laurentlonchay@gmail.com>
2021-03-26 17:03:49 +0100

    11 | In Progress | To test

---
+++ api/src/main/java/com/switchfully/curiosity/digibooky/api/dtos/CreateDtoLoan.java
@@ -0,0 +1,20 @@
+package com.switchfully.curiosity.digibooky.api.dtos;
+
+public class CreateDtoLoan {
+
+    private String userId;
+    private String bookIsbn;
+
+    public CreateDtoLoan(String userId, String bookIsbn) {
+        this.userId = userId;
+        this.bookIsbn = bookIsbn;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public String getBookIsbn() {
+        return bookIsbn;
+    }
+}
